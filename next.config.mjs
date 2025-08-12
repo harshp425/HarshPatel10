dc826510ac653b6f9f5a09ebfca36843705703f7
Harsh Patel <126726290+harshp425@users.noreply.github.com>
2025-08-12 12:32:13 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/harshpatel10'
+// };
 const nextConfig = {
   output: 'export',
-  basePath: '/harshpatel10'
+
 };
 
 export default nextConfig;
