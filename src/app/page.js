c62a6ea5cb4eceb5c98035fc74542c90f7e7e51c
Harsh Patel <126726290+harshp425@users.noreply.github.com>
2025-08-12 13:08:46 -0400

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -335,7 +335,7 @@ export default function ModernPortfolio() {
                     <span className="ml-2 text-slate-600">3.66</span>
                   </div>
                   <div>
-                    <span className="font-semibold text-slate-800">Dean's List:</span>
+                    <span className="font-semibold text-slate-800">Dean&apos;s List:</span>
                     <span className="ml-2 text-slate-600">Spring '23, Fall '23, Spring '24, Fall '24, Spring '25</span>
                   </div>
                 </div>
